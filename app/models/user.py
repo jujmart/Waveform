f2f91a71cd33bc245ef6568b9aaed95057050073
Justice Martin <jujmart12@gmail.com>
2021-08-15 17:08:04 -0400

    Updated users table and deleted migration folder

--- app/models/user.py
+++ app/models/user.py
@@ -1,23 +1,30 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(256), nullable=False, unique=True)
+    hashedPassword = db.Column(db.String(255), nullable=False)
+    profilePhotoUrl = db.Column(db.String(
+        500), nullable=False, default="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")  # will add this photo to AWS and update this url to the created AWS url
+    createdAt = db.Column(db.DateTime(timezone=True),
+                          nullable=False, server_default=func.now())
+    updatedAt = db.Column(db.DateTime(timezone=True),
+                          nullable=False, onupdate=func.now())
 
     @property
     def password(self):
-        return self.hashed_password
+        return self.hashedPassword
 
     @password.setter
     def password(self, password):
-        self.hashed_password = generate_password_hash(password)
+        self.hashedPassword = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
@@ -26,5 +33,6 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'profilePhotoUrl': self.profilePhotoUrl
         }
