35d973457fdecdaa45624e2d8d567e5a9fe71c37
Justice Martin <jujmart12@gmail.com>
2021-09-21 18:56:43 -0400

    Populated information on the page for the search

--- react-app/src/components/Search/SearchPopulated.js
+++ react-app/src/components/Search/SearchPopulated.js
@@ -5,6 +5,7 @@ const SearchPopulated = () => {
 
 	return (
 		<div>
+			<h1>POPULATED</h1>
 			{search.songs &&
 				Object.values(search.songs).map((song) => (
 					<div key={song.id}>{song.title}</div>
@@ -12,6 +13,14 @@ const SearchPopulated = () => {
 			{search.playlists &&
 				Object.values(search.playlists).map((playlist) => (
 					<div key={playlist.id}>{playlist.title}</div>
+				))}
+			{search.users &&
+				Object.values(search.users).map((user) => (
+					<div key={user.id}>{user.username}</div>
+				))}
+			{search.genres &&
+				Object.values(search.genres).map((genre) => (
+					<div key={genre.id}>{genre.genreName}</div>
 				))}
 		</div>
 	);
