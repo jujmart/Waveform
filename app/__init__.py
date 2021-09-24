f4febdc61e5e77d193b8f2ecf6f419c4d4a7d6ad
Justice Martin <jujmart12@gmail.com>
2021-09-24 15:25:19 -0400

    Added followed users to right nav bar

--- app/__init__.py
+++ app/__init__.py
@@ -82,6 +82,6 @@ def inject_csrf_token(response):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+    if path == 'favicon.png':
+        return app.send_static_file('favicon.png')
     return app.send_static_file('index.html')
