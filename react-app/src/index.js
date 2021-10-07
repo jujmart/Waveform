bd741e105b5c91ed21bb59c433553b88778449a3
Justice Martin <jujmart12@gmail.com>
2021-10-06 20:37:51 -0400

    removed context menu

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
-import configureStore from './store';
-import { ModalProvider } from './context/Modal';
-import { ContextMenuProvider } from './context/ContextMenu/ContextMenuModal/ContextMenu';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+import { ModalProvider } from "./context/Modal";
+// import { ContextMenuProvider } from "./context/ContextMenu/ContextMenuModal/ContextMenu";
 
 const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ContextMenuProvider>
-        <ModalProvider>
-          <App />
-        </ModalProvider>
-      </ContextMenuProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			{/* <ContextMenuProvider> */}
+			<ModalProvider>
+				<App />
+			</ModalProvider>
+			{/* </ContextMenuProvider> */}
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
