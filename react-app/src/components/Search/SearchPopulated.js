3a0c43061852b5b24d79cd8704515ee41b665116
Justice Martin <jujmart12@gmail.com>
2021-10-07 20:08:13 -0400

    Fixed the positioning of the search populated div

--- react-app/src/components/Search/SearchPopulated.js
+++ react-app/src/components/Search/SearchPopulated.js
@@ -8,37 +8,45 @@ const SearchPopulated = () => {
 	const users = useSelector((state) => state.users);
 
 	return (
-		<div>
-			<h1>POPULATED</h1>
-			{search.songs.map((songId) => (
-				<div className="playlist-song-container_div" key={songId}>
-					<Song songId={songId} />
-				</div>
-			))}
-			{search.playlists.map((playlistId) => (
-				<PlaylistCard playlistId={playlistId} key={playlistId} />
-			))}
-			{search.users.map((userId) => (
-				<Link to={`/users/${userId}`} key={userId}>
-					<div key={userId} className="newest-song-container_div">
-						<img
-							className="song-activity-album_img"
-							src={users[userId]?.profilePhotoUrl}
-							alt="Friend Img"
-						/>
-						<p className="song-activity-song_p">
-							{users[userId]?.username}
-						</p>
-						<p className="song-activity-album_p">
-							Joined On:{" "}
-							{users[userId]?.createdAt
-								?.split(" ")
-								.splice(1, 3)
-								.join(" ")}
-						</p>
+		<div id="search-populated-container">
+			<div>
+				<h2>Songs</h2>
+				{search.songs.map((songId) => (
+					<div className="playlist-song-container_div" key={songId}>
+						<Song songId={songId} />
 					</div>
-				</Link>
-			))}
+				))}
+			</div>
+			<div>
+				<h2>Playlists</h2>
+				{search.playlists.map((playlistId) => (
+					<PlaylistCard playlistId={playlistId} key={playlistId} />
+				))}
+			</div>
+			<div>
+				<h2>Users</h2>
+				{search.users.map((userId) => (
+					<Link to={`/users/${userId}`} key={userId}>
+						<div key={userId} className="newest-song-container_div">
+							<img
+								className="song-activity-album_img"
+								src={users[userId]?.profilePhotoUrl}
+								alt="Friend Img"
+							/>
+							<p className="song-activity-song_p">
+								{users[userId]?.username}
+							</p>
+							<p className="song-activity-album_p">
+								Joined On:{" "}
+								{users[userId]?.createdAt
+									?.split(" ")
+									.splice(1, 3)
+									.join(" ")}
+							</p>
+						</div>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 };
