38b6ef46796c659f0f25e071a669dcca188700a1
Justice Martin <jujmart12@gmail.com>
2021-10-08 15:37:06 -0400

    Added conditional headers for songs, playlists, users sections

--- react-app/src/components/Search/SearchPopulated.js
+++ react-app/src/components/Search/SearchPopulated.js
@@ -9,44 +9,64 @@ const SearchPopulated = () => {
 
 	return (
 		<div id="search-populated-container">
-			<div>
-				<h2>Songs</h2>
-				{search.songs.map((songId) => (
-					<div className="playlist-song-container_div" key={songId}>
-						<Song songId={songId} />
-					</div>
-				))}
-			</div>
-			<div>
-				<h2>Playlists</h2>
-				{search.playlists.map((playlistId) => (
-					<PlaylistCard playlistId={playlistId} key={playlistId} />
-				))}
-			</div>
-			<div>
-				<h2>Users</h2>
-				{search.users.map((userId) => (
-					<Link to={`/users/${userId}`} key={userId}>
-						<div key={userId} className="newest-song-container_div">
-							<img
-								className="song-activity-album_img"
-								src={users[userId]?.profilePhotoUrl}
-								alt="Friend Img"
-							/>
-							<p className="song-activity-song_p">
-								{users[userId]?.username}
-							</p>
-							<p className="song-activity-album_p">
-								Joined On:{" "}
-								{users[userId]?.createdAt
-									?.split(" ")
-									.splice(1, 3)
-									.join(" ")}
-							</p>
+			{search.songs.length ? (
+				<div>
+					<h2 className="search-populated_header">Songs</h2>
+					{search.songs.map((songId) => (
+						<div
+							className="playlist-song-container_div"
+							key={songId}
+						>
+							<Song songId={songId} />
 						</div>
-					</Link>
-				))}
-			</div>
+					))}
+				</div>
+			) : null}
+			{search.playlists.length ? (
+				<div>
+					<h2 className="search-populated_header">Playlists</h2>
+					{search.playlists.map((playlistId) => (
+						<PlaylistCard
+							playlistId={playlistId}
+							key={playlistId}
+						/>
+					))}
+				</div>
+			) : null}
+			{search.users.length ? (
+				<div>
+					<h2 className="search-populated_header">Users</h2>
+					{search.users.map((userId) => (
+						<Link to={`/users/${userId}`} key={userId}>
+							<div
+								key={userId}
+								className="newest-song-container_div"
+							>
+								<img
+									className="song-activity-album_img"
+									src={users[userId]?.profilePhotoUrl}
+									alt="Friend Img"
+								/>
+								<p className="song-activity-song_p">
+									{users[userId]?.username}
+								</p>
+								<p className="song-activity-album_p">
+									Joined On:{" "}
+									{users[userId]?.createdAt
+										?.split(" ")
+										.splice(1, 3)
+										.join(" ")}
+								</p>
+							</div>
+						</Link>
+					))}
+				</div>
+			) : null}
+			{!search.songs.length &&
+			!search.playlists.length &&
+			!search.users.length ? (
+				<h2 className="search-populated_header">No results found</h2>
+			) : null}
 		</div>
 	);
 };
